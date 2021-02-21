83dfef971d6ba13b5b20b56b98048de5de6aeb08
t203334 <shimakura.tomoshi.ao@tut.jp>
2021-02-21 22:13:47 +0900

    assignment2

---
+++ SDM-report2/calc_mul.py
@@ -0,0 +1,34 @@
+#!/usr/bin/python3
+
+import re
+                
+def calc(A,B):
+        ai=str(A)
+        bi=str(B)
+        p = re.compile('\d+(\.\d+)?')
+        if p.match(ai) or p.match(bi):
+                a=float(ai)
+                b=float(bi)
+                if 0<a and a<b and b<1000:
+                        valid=True
+                else:
+                        valid=False
+        else:
+                valid=False
+                
+        if valid:
+                ans=a*b
+                return ans
+        else:
+                return -1
+        
+                
+def main ():
+	matchstring = ''
+	while matchstring != 'end':
+                A = input ('input A: ')
+                B = input ('input B: ')
+                print ('input A * input B = ', calc(A,B))
+
+if __name__ == '__main__':
+	main()
