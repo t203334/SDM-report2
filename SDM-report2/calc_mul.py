4da513f803f6c21ef8980bb90a5964476ffefdf1
t203334 <shimakura.tomoshi.ao@tut.jp>
2021-02-22 03:35:25 +0900

    assignment2

--- SDM-report2/calc_mul.py
+++ SDM-report2/calc_mul.py
@@ -1,18 +1,36 @@
 #!/usr/bin/python3
-import re
-def calc(A,B): 
- ai=str(A) 
- bi=str(B) 
- p = re.compile('¥d+(¥.¥d+)?') 
- if p.match(ai) or p.match(bi): 
- a=float(ai) 
- b=float(bi) 
 
-def main (): 
-matchstring = '' 
-while matchstring != 'end': 
- A = input ('input A: ') 
- B = input ('input B: ') 
- print ('input A * input B = ', calc(A,B)) 
-if __name__ == '__main__': 
-main()
+import re
+                
+def calc(A,B):
+        ai=str(A)
+        bi=str(B)
+        p = re.compile('\d+(\.\d+)?')
+        if p.match(ai) and p.match(bi):
+                a=float(ai)
+                b=float(bi)
+                
+                if 0<a and a<1000 and 0<b and b<1000 and(a.is_integer()==True)and (b.is_integer()==True):
+                        valid=True
+                else:
+                        valid=False
+        else:
+                valid=False
+                
+        if valid:
+                ans=a*b
+                return ans
+        else:
+                return -1
+        
+                
+def main ():
+	matchstring = ''
+	while matchstring != 'end':
+                A = input ('input A: ')
+                B = input ('input B: ')
+                print ('input A * input B = ', calc(A,B))
+
+
+if __name__ == '__main__':
+	main()
